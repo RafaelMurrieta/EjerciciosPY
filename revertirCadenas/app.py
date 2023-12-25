6c260965d82fdc74261e5347180de8589b08fd8e
Rafael Murrieta <r7121005846@gmail.com>
2023-12-25 17:36:48 -0600

    Nuevos ejercicios

--- revertirCadenas/app.py
+++ revertirCadenas/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def revertir(palabra: str):
     print(palabra)
     newPalabra = []
@@ -7,14 +6,4 @@ def revertir(palabra: str):
     resultado = ''.join(newPalabra)
     print(resultado)
     
-=======
-def revertir(palabra: str):
-    print(palabra)
-    newPalabra = []
-    for i in range(len(palabra)-1, -1 , -1):
-        newPalabra.append(palabra[i])
-    resultado = ''.join(newPalabra)
-    print(resultado)
-    
->>>>>>> cca8414323dade681936e1d599d1daac71b40e7a
 revertir("Hola mundo")
